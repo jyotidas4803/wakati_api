d5fd7862b88d8f2830937e8a6c023ca265bbffd9
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-01-08 08:50:38 +0000

    pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { notFound, onError } from 'stoker/middlewares'
+import { pinoLogger } from 'hono-pino'
 
 
 const app = new OpenAPIHono()
+app.use(pinoLogger(
+  {
+    http:{
+      reqId: ()=>crypto.randomUUID()
+    }
+  }
+))
 
 app.notFound(notFound)
 app.onError(onError)
 
 app.get('/', (c) => {
+  c.var.lo("")
   return c.text('Hello Hono!')
 })
 
