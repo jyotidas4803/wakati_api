ff8232fdf85e0fd81725ff8e71cbfd9f3e396578
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2025-01-08 08:13:07 +0000

    added stoker

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { notFound, onError } from 'stoker/middlewares'
 
 
 const app = new OpenAPIHono()
 
+app.notFound(notFound)
+app.onError(onError)
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/error',(c) => {
+  throw new Error('Custom 404 Message')
 })
 
 export default app
